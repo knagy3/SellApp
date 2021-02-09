5ed97580e210068a248fe95eab6ae30a597c2cbb
Kristof Richard Nagy <knagy3@jaguarlandrover.com>
2021-02-09 22:12:58 +0100

    login is in progress

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, StyleSheet, View} from 'react-native';
 import Welcome from './app/screens/Welcome';
 import ViewImage from './app/screens/ViewImage';
@@ -13,24 +13,36 @@ import Account from './app/screens/Account';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
 import Screen from './app/components/Screen';
+import AppPicker from './app/components/AppPicker';
+import Login from './app/screens/Login';
+
+const categories = [
+  {label: "Furniture", value: 1 },
+  {label: "Clothing", value: 2 },
+  {label: "Cameras", value: 3 },
+];
 
 // import Picture from './assets/icon.png' did not work!
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
 
   return (
-    // <Welcome/>
-    <Screen>
-      <AppTextInput placeholder="Username" icon="email"/>
-    </Screen>
-    
-    //<ListingDetails />
-    
+    <Login/>  
   );
 }
 
 // Validates the property, inline style (style={{}}), does not.
 // If you do a mistake you will not have an error without sylesheet
 
+{/* <Screen>
+  <AppPicker 
+    selectedItem={category}
+    onSelectItem={item=>setCategory(item)}
+    items={categories}
+    icon="apps" 
+    placeholder="Username"/>
+  <AppTextInput icon="email" placeholder="Email"/>
+</Screen> */}
 
 // const styles = StyleSheet.create({
 //   container: {
---
+++ app/screens/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import AppButton from '../components/AppButton';
+import AppTextInput from '../components/AppTextInput';
+import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password"),
+})
+
+function Login(props) {
+
+    return (
+        <Screen style={styles.container}>
+            <Image style={styles.logo} source={require('../../assets/logo.jpg')}/>
+            
+            <Formik
+                initialValues={{ email: '', password: ''}}
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+            >
+                {({ handleChange, handleSubmit, errors}) => (
+                    <>
+                        <AppTextInput 
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="email"
+                            keyboardType="email-address"
+                            onChangeText={handleChange('email')}
+                            placeholder="Email"
+                        />
+                        <AppText style={{color:'red'}}>{errors.email}</AppText>
+                        <AppTextInput 
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="lock"
+                            onChangeText={handleChange('password')}
+                            secureTextEntry={true}
+                            placeholder="Password"
+                        />
+                        <AppText style={{color:'red'}}>{errors.password}</AppText>
+                        <AppButton title="Login" onPress={handleSubmit}/>
+                    </>
+                )}
+            </Formik>
+        </Screen>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    logo: {
+        width: 80,
+        height: 80,
+        alignSelf: 'center',
+        marginTop: 50,
+        marginBottom: 20,
+    },
+})
+export default Login;
